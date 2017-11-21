788639cc60f6468aa6726e3c227f7993c512c48a
Jessica Sena <jessisena@gmail.com>
2017-11-21 15:23:53 +0100

    first commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  template: `
+  <p>Im a user component</p>
+  `
+})
+
+export class UsersComponent {
+    text = 'This is a users component';
+}
